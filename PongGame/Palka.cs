8a0c515c16b7aebb850bdb244458bc4b33975f69
Bojan <bojantrpeski123@gmail.com>
2022-06-16 13:13:24 +0200

    Завршен почетниот код за класата палка

--- PongGame/Palka.cs
+++ PongGame/Palka.cs
@@ -9,7 +9,28 @@ namespace PongGame
 {
     public class Palka
     {
-        public Color color { get; set; }
+        public Color Color { get; set; }= Color.Black;
         public Point Location { get; set; }
+        public static int Width { get; set; } = 10;
+        public static int Height { get; set; } = 30;
+        public Palka(Point p)
+        {
+            Location = p;
+        }
+        public void Draw(Graphics g)
+        {
+            Brush b = new SolidBrush(Color);
+            g.FillRectangle(b, Location.X, Location.Y, Width, Height);
+            b.Dispose();
+        }
+        public void MoveUp()
+        {
+            Location=new Point(Location.X, Location.Y+5);
+        }
+        public void MoveDown()
+        {
+            Location = new Point(Location.X, Location.Y - 5);
+        }
+
     }
 }
